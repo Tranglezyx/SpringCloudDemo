35cb5b737a20e204037682cc8e2ad6d443cd1b86
Trangle <407833@qq.com>
2021-09-15 10:17:15 +0800

    add

--- common-starter/src/main/java/com/trangle/common/config/CommonConfig.java
+++ common-starter/src/main/java/com/trangle/common/config/CommonConfig.java
@@ -1,7 +1,6 @@
 package com.trangle.common.config;
 
 import com.trangle.common.filter.TokenFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- common-starter/src/main/java/com/trangle/common/filter/TokenFilter.java
+++ common-starter/src/main/java/com/trangle/common/filter/TokenFilter.java
@@ -1,6 +1,8 @@
 package com.trangle.common.filter;
 
 import org.apache.catalina.connector.RequestFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import java.io.IOException;
@@ -10,10 +12,20 @@ import java.io.IOException;
  */
 public class TokenFilter implements Filter {
 
+    private static final Logger log = LoggerFactory.getLogger(TokenFilter.class);
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("请求路径为： -- " + ((RequestFacade) servletRequest).getRequestURI());
+        long startTime = System.currentTimeMillis();
+        if (log.isInfoEnabled()) {
+            log.info("请求路径为： -- {}", ((RequestFacade) servletRequest).getRequestURI());
+        }
+
         filterChain.doFilter(servletRequest, servletResponse);
+        if (log.isInfoEnabled()) {
+            long endTime = System.currentTimeMillis();
+            log.info("请求时间为： -- {}ms", endTime - startTime);
+        }
     }
 
     @Override
