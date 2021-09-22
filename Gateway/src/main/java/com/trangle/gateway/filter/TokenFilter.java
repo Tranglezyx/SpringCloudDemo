c44955f0fc5326eb0c8e743faf68614f7f5465d3
trangle <407833@qq.com>
2021-09-22 18:40:49 +0800

    新增全局路由权限校验;

--- Gateway/src/main/java/com/trangle/gateway/filter/TokenFilter.java
+++ Gateway/src/main/java/com/trangle/gateway/filter/TokenFilter.java
@@ -1,7 +1,8 @@
 package com.trangle.gateway.filter;
 
-import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -10,10 +11,20 @@ import reactor.core.publisher.Mono;
  * @author Trangle
  */
 @Component
-public class TokenFilter implements GatewayFilter {
+public class TokenFilter implements GlobalFilter {
+
+    private static final String TOKEN = "Authorization";
+    private static final String TOKEN_VALUE = "Bearer 123456";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange);
+        if (CollectionUtils.isEmpty(exchange.getRequest().getHeaders().get(TOKEN))) {
+            throw new RuntimeException("请先登录");
+        }
+        String token = exchange.getRequest().getHeaders().get(TOKEN).stream().findFirst().orElse(null);
+        if (TOKEN_VALUE.equals(token)) {
+            return chain.filter(exchange);
+        }
+        throw new RuntimeException("请先登录");
     }
 }
