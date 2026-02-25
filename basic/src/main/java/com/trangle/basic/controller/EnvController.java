47ab23b473e267752a3294f65b6776bd981bddc2
zhouyunxiang <tranglezyx@163.com>
2026-02-25 18:26:27 +0800

    add;

---
+++ basic/src/main/java/com/trangle/basic/controller/EnvController.java
@@ -0,0 +1,19 @@
+package com.trangle.basic.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/env")
+@RestController
+public class EnvController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @PostMapping("/getName")
+    public String name(){
+        return applicationName;
+    }
+}
